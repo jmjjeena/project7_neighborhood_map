f5441b530375fc6cf02a362719197f9b6e5292d9
Jeena <jmj.jeena@gmail.com>
2018-10-31 15:39:48 -0700

    Added MapContainer State.

--- src/Components/MapContainer.js
+++ src/Components/MapContainer.js
@@ -2,41 +2,47 @@ import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 
-export class MapContainer extends Component {
+class MapContainer extends Component { {
 
     state = {
-        places: [
-            {
-                title: "Comedy Store",
-                location: {
-                    lat: 34.095169,
-                    lng: -118.373918
-                }
-            }
-        ]
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {}
+    };
 
+    onMarkerClick = (props, marker) => {
+        console.log('Props: ', props)
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+        });
     }
 
     render() {
         const style = {
             width: '100%',
             height: '100%'
-        }
-      
-
+        };
+    
         return (
-            <Map google={this.props.google} zoom={14} style={style} initialCenter={this.state.places[0].location}>
-                {this.state.places.map((place, index) =>
+            <Map
+                google={this.props.google}
+                zoom={14}
+                style={style}
+                initialCenter={this.props.places[0].location}>
+                {this.props.places.map((place, index) => 
                     <Marker
-                        position={place.location}
-                        title={place.title}
+                        position={{ lat: place.location.lat, lng: place.location.lng }}
                         key={index}
+                        onClick={this.onMarkerClick}
                     />
                 )}
             </Map>
+            
         )
     }
 }
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyCVjG1_rpwST332EGF3YRDaSO0ez-ws_aw'
-})(MapContainer)
+})(MapContainer);
